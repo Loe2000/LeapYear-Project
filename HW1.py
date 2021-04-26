a6a45d2eb1afc7111c0b55a4752eb000a8e77341
Loe2000 <loe@oregonstate.edu>
2021-04-25 23:19:10 -0700

    Handles the case where a non-integer value is inputted

--- HW1.py
+++ HW1.py
@@ -1,12 +1,20 @@
 # Takes a year as an input from the user
-year = int(input("Give me a year: "))
+
+# Handles non-integer values
+i = 0
+while i == 0:
+    try:
+        year = int(input("Give me a year. (Input an integer): "))
+        i = 1
+    except:
+        print("Sorry, that isn't a valid year")
 
 # calculates whether that year is a leap year and outputs it
 if (year % 400) == 0:
     print(year, "is a leap year")
 elif (year % 4) == 0:
     if (year % 100) == 0:
-         print(year, "is not a leap year")
+        print(year, "is not a leap year")
     else:
         print(year, "is a leap year")
 else:
